ebad6257ff01844943c764796ed7d45b1799636e
Giovanni <g.salmeri@mondodomani.org>
2021-12-14 20:46:54 +0100

    Simplification

--- chant.js
+++ chant.js
@@ -3,18 +3,15 @@ document.addEventListener("DOMContentLoaded", function() {
     var chants = document.querySelectorAll("pre.chant");
     chants.forEach(function(chant) {
         chant.firstChild.style.display = 'block'; // important
-        var layoutChant = function() {
+        var ctxt = new exsurge.ChantContext();
+        var mappings = exsurge.Gabc.createMappingsFromSource(ctxt, chant.firstChild.textContent);
+        var score = new exsurge.ChantScore(ctxt, mappings, true);
+        window.addEventListener("resize", function() {
           score.performLayoutAsync(ctxt, function() {
             score.layoutChantLines(ctxt, chant.firstChild.offsetWidth, function() {
               chant.firstChild.innerHTML = score.createSvg(ctxt);
             });
           });
-        }
-        var ctxt = new exsurge.ChantContext();
-        var mappings = exsurge.Gabc.createMappingsFromSource(ctxt, chant.firstChild.textContent);
-        var score = new exsurge.ChantScore(ctxt, mappings, true);
-        window.addEventListener("resize", function() {
-            layoutChant();
         });
     });
 }, false);

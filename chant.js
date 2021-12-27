895e665e4652fc592d85eada7c6ca805f4a10017
Giovanni <g.salmeri@mondodomani.org>
2021-12-28 00:03:39 +0100

    Color of score customisable

--- chant.js
+++ chant.js
@@ -1,9 +1,16 @@
 "use strict";
 document.addEventListener("DOMContentLoaded", function() {
     var chants = document.querySelectorAll("pre.chant");
+    if (chants) var color = window.getComputedStyle(chants[0]).getPropertyValue("color");
     chants.forEach(function(chant) {
-        chant.firstChild.style.display = 'block'; // important
+        chant.firstChild.style.display = "block"; // important
         var ctxt = new exsurge.ChantContext();
+        ctxt.dropCapTextColor = color;
+        ctxt.lyricTextColor = color;
+        ctxt.rubricColor = "#d00";
+        ctxt.staffLineColor = color;
+        ctxt.neumeLineColor = color;
+        ctxt.dividerLineColor = color;
         var mappings = exsurge.Gabc.createMappingsFromSource(ctxt, chant.firstChild.textContent);
         var score = new exsurge.ChantScore(ctxt, mappings, true);
         window.addEventListener("resize", function() {

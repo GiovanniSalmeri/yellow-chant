5c3be783374aaf3f2b9597e213b5e49603a68da7
Giovanni <g.salmeri@mondodomani.org>
2021-12-30 13:45:21 +0100

    Added aria-describedby

--- chant.js
+++ chant.js
@@ -1,7 +1,17 @@
 "use strict";
 document.addEventListener("DOMContentLoaded", function() {
     var chants = document.querySelectorAll("pre.chant");
-    chants.forEach(function(chant) {
+    chants.forEach(function(chant, i) {
+        // accessibility
+        var chantText = chant.firstChild.textContent;
+        var accessibleText = document.createElement("PRE");
+        accessibleText.style.display = "none";
+        accessibleText.id = "chant"+i;
+        accessibleText.textContent = chantText;
+        chant.insertAdjacentElement("afterend", accessibleText);
+        chant.setAttribute("role", "img");
+        chant.setAttribute("aria-describedby", "chant"+i);
+        // draw score
         chant.firstChild.style.display = "block"; // important
         var firstColor = window.getComputedStyle(chant, "::first-letter").color;
         var color = window.getComputedStyle(chant).color;
@@ -12,7 +22,7 @@ document.addEventListener("DOMContentLoaded", function() {
         ctxt.staffLineColor = color;
         ctxt.neumeLineColor = color;
         ctxt.dividerLineColor = color;
-        var mappings = exsurge.Gabc.createMappingsFromSource(ctxt, chant.firstChild.textContent);
+        var mappings = exsurge.Gabc.createMappingsFromSource(ctxt, chantText);
         var score = new exsurge.ChantScore(ctxt, mappings, true);
         window.addEventListener("resize", function() {
           score.performLayoutAsync(ctxt, function() {
